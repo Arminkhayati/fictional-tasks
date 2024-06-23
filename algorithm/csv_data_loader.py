6be79e454710e4b53a202e8335acc25e46fb187b
Armin Khayati <Northatlas@gmail.com>
2024-06-23 17:47:52 -0400

    added test2 notebook

--- algorithm/csv_data_loader.py
+++ algorithm/csv_data_loader.py
@@ -86,61 +86,6 @@ class CSVDataLoader:
 
         return X, Y, Y_transform, W, W_transform, self.time_all, self.event_all
 
-    def get_balanced_data(self, Tmax=7500, num_intervals=7, k=1500, random_state=1):
-        """
-            Select at most k random items for each class in an imbalanced dataset with a random state.
-        """
-        np.random.seed(random_state)
-        X = self.data.drop(['Survival_Time', 'indicater'], axis=1)
-        self.time_all = self.data['Survival_Time'].values
-        self.event_all = self.data['indicater'].values
-        # max_time = self.data['Survival_Time'].max()
-
-        intervals = [(i * (Tmax // num_intervals), (i + 1) * (Tmax // num_intervals)) for i in range(num_intervals)]
-        Y = np.zeros((len(self.time_all), num_intervals), dtype=np.int_)
-        # Until the event happens, value is 1. after that, it is 0
-        for i, time_val in enumerate(self.time_all):
-            for j, (left, right) in enumerate(intervals):
-                if time_val > right or (left < time_val <= right):
-                    Y[i, j] = 1
-
-
-        Y_summed = Y.sum(axis=1)
-        unique_classes = np.unique(Y_summed)
-        selected_indices = []
-        # Get unique classes in the dataset
-        for cls in unique_classes:
-            # Get indices for all items belonging to the current class
-            class_indices = np.where(Y_summed == cls)[0]
-            # Randomly shuffle the indices
-            class_indices = np.random.permutation(class_indices)
-            # Select at most k items
-            selected_indices.extend(class_indices[:k])
-
-        Y = torch.Tensor(Y[selected_indices])
-
-        # Creat mask matrix
-        W = np.zeros((len(self.time_all), num_intervals), dtype=np.int_)
-        # Until the time we know he was alive the value is 1, after that is 0
-        for i, (time_val, event_val) in enumerate(zip(self.time_all, self.event_all)):
-            for j, (left, right) in enumerate(intervals):
-                if event_val == 0 and time_val < left:
-                    W[i, j] = 0
-                else:
-                    W[i, j] = 1
-        W = torch.Tensor(W[selected_indices])
-
-        for col in X.columns:
-            if X[col].dtype == bool:
-                X[col] = X[col].astype(int)
-        X = torch.tensor(X.values[selected_indices], dtype=torch.float32)
-
-        Y_transform = [Y[:, i:i + 1] for i in range(Y.size(1))]
-        W_transform = [W[:, i:i + 1] for i in range(W.size(1))]
-
-        return X, Y, Y_transform, W, W_transform, self.time_all[selected_indices], self.event_all[selected_indices]
-
-
 
 
 
